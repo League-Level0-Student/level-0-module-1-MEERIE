5ce7c10f8fda3bd9f4a34177345d1a43d52476a0
League Student <league@iMac.attlocal.net>
2019-05-29 18:28:04 -0700

    secretmessagebox ip

--- src/Remarkable.java
+++ src/Remarkable.java
@@ -1,4 +1,9 @@
+import javax.swing.JOptionPane;
 
 public class Remarkable {
-
+public static void main(String[] args) {
+	String name = JOptionPane.showInputDialog ("input your name kid");
+	JOptionPane.showMessageDialog( null, "you are very remarkable " + name );
+	JOptionPane.showMessageDialog(null, "But then again. I know u r an ugly turd");
+}
 }
---
+++ src/SecretMessageBox.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class SecretMessageBox {
+public static void main(String[] args) {
+	String password = "123";
+	String thingy = JOptionPane.showInputDialog("enter an amount of cash plz");
+	String pin = JOptionPane.showInputDialog( " plz enter your pin ");
+	if (pin.equals(password)) {
+		JOptionPane.showMessageDialog(null,"you have $" + thingy + " in your account");
+		JOptionPane.showMessageDialog(null,"it seems you have been hacked");
+		JOptionPane.showMessageDialog(null,"pay 100000000000000000000 to ...");
+		JOptionPane.showMessageDialog(null,"go to HELL");
+
+
+
+
+	}
+	else play(https://www.google.com/doodles/halloween-2018);
+}
+}
